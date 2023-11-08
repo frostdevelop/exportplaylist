d6a11ce1abfde09c531e487f3b27e0a8b4e974dc
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-11-08 07:01:52 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,8 @@
-
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if(tab.url && (tab.url.includes("youtube.com/watch")){
+    
+  }
+  else if(tab.url && (tab.url.includes("youtube.com/playlist")){
+    
+  }
+}
