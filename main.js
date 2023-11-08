d546c98a973a4afdbb9209f7ea3a72e32f1c8176
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-11-08 11:34:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const fileInput = document.getElementById('inputfile');
+let title = false;
+let listname = "";
+
 fileInput.addEventListener('change', (event) => {
     const files = event.target.files;
     const fr=new FileReader();
@@ -11,3 +14,35 @@ fileInput.addEventListener('change', (event) => {
     }
     fr.readAsText(files[0]);
 });
+
+function grey(id){
+    document.getElementById(id).classList.add("disabled");
+}
+
+function ungrey(id){
+    document.getElementById(id).classList.remove("disabled");
+}
+
+document.getElementById('bsdown').addEventListener("click",(e)=>{
+    chrome.tabs.sendMessage()
+},false)
+
+document.getElementById('beplay').addEventListener("click",(e)=>{
+    chrome.tabs.sendMessage()
+},false)
+
+document.getElementById('cexti').addEventListener("change",(e)=>{
+    title = !title;
+},false)
+
+document.getElementById('listname').addEventListener("change",(e)=>{
+    listname = document.getElementById('listname').value;
+},false)
+
+document.getElementById('bstarti').addEventListener("click",(e)=>{
+    chrome.tabs.sendMessage()
+},false)
+
+chrome.runtime.onMessage.addListener((obj, sender, res)=>{
+    
+}
