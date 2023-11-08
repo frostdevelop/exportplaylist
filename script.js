5337dad62a55d66bf30f1b66bf402c9ca26c148f
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-11-07 18:03:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-let goToBottom = setInterval(() => window.scrollBy(0, 1000), 100);
-
-clearInterval(goToBottom);
-let videos = [];
-const links = document.querySelectorAll('a');
-for (const link of links) {
-    if (link.id === "video-title") {
-        link.href = link.href.split('&list=')[0];
-        videos.push(link.title + ',' + link.href);
-    }
+//Export
+function scroll(){
+    var goToBottom = setInterval(() => window.scrollBy(0, 2000), 200);
 }
 
-clearInterval(goToBottom);
-let videos = [];
-const links = document.querySelectorAll('a');
-for (const link of links) {
-    if (link.id === "video-title") {
-        link.href = link.href.split('&list=')[0];
-        videos.push(link.href);
+function export(title){
+    clearInterval(goToBottom);
+    let videos = [];
+    const links = document.querySelectorAll('a');
+    for (const link of links) {
+        if (link.id === "video-title") {
+            link.href = link.href.split('&list=')[0];
+            videos.push(title ? link.title + ',' + link.href : link.href);
+        }
     }
+
+    let data = videos.join('\n');
+    let blob = new Blob([data], {type: 'text/csv'});
+    let elem = window.document.createElement('a');
+    elem.href = window.URL.createObjectURL(blob);
+    elem.download = 'my_data.csv';
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
 }
 
-let data = videos.join('\n');
-let blob = new Blob([data], {type: 'text/csv'});
-let elem = window.document.createElement('a');
-elem.href = window.URL.createObjectURL(blob);
-elem.download = 'my_data.csv';
-document.body.appendChild(elem);
-elem.click();
-document.body.removeChild(elem);
-
-// Add import code
-
+//Import
 function waitForElm(selector) {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
@@ -51,13 +45,16 @@ function waitForElm(selector) {
     });
 }
 
-document.querySelectorAll('[aria-label="Save to playlist"]')[0].click()
-waitForElm('tp-yt-paper-dialog').then(()=>{
-// document.getElementsByClassName('style-scope ytd-playlist-add-to-option-renderer')[0].click()
-let checks = document.querySelectorAll('tp-yt-paper-checkbox[class="style-scope ytd-playlist-add-to-option-renderer"]')
-for (let i=0;i<checks.length;i++){
-  if(checks[i].childNodes[2].children[0].children[0].children[0].title=="Watch later"){
-    checks[i].click()
-  }
+function savePlaylist(listname){
+    document.querySelectorAll('[aria-label="Save to playlist"]')[0].click()
+    waitForElm('tp-yt-paper-dialog').then(()=>{
+    let checks = document.querySelectorAll('tp-yt-paper-checkbox[class="style-scope ytd-playlist-add-to-option-renderer"]')
+    for (let i=0;i<checks.length;i++){
+      if(checks[i].childNodes[2].children[0].children[0].children[0].title==listname){
+        checks[i].click()
+      }
+    }
+    }
+    );
 }
-})
+
