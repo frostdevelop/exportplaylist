506466826d7cf58db62bf1cdd34e3b2dd89362bc
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-11-07 20:47:02 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-window.showOpenFilePicker()
+const fileInput = document.getElementById('inputfile');
+fileInput.addEventListener('change', (event) => {
+    const files = event.target.files;
+    const fr=new FileReader();
+    fr.onload = () =>{
+      let data = fr.result;
+      let arr = data.split('\n');
+      for(let i=0;i<arr.length;i++){
+        let w = window.open(arr[i]);
+      }
+    }
+    fr.readAsText(files[0]);
+});
