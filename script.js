c3aa5e8cf98de998b439b9923b070cc4d016752e
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-11-17 16:26:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Export
-let url;
+let url = location.href.split("?")[0];
 let page;
 
 function scroll(){
@@ -26,7 +26,7 @@ function exportPlay(title){
     let blob = new Blob([data], {type: 'text/csv'});
     let elem = window.document.createElement('a');
     elem.href = window.URL.createObjectURL(blob);
-    elem.download = 'my_data.csv';
+    elem.download = '.csv';
     document.body.appendChild(elem);
     elem.click();
     document.body.removeChild(elem);
@@ -99,7 +99,7 @@ function injectplaylist(title){
         icondiv.appendChild(yticon);
         yticon.appendChild(yticonshape);
         yticonshape.innerHTML = '<icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z"/></svg></div></icon-shape>';
-        scroll = document.createElement('button');
+        let scroll = document.createElement('button');
         scroll.innerHtml = "Load videos";
         scroll.onclick = scroll();
         buttonrenderer = document.createElement("ytd-button-renderer");
@@ -122,17 +122,31 @@ function injectplaylist(title){
     });
 }
 
+if(location.href.split("?")[0]==="https://www.youtube.com/playlist"){
+  page = "playlist";
+  injectplaylist(false);
+}
+else if(location.href.split("?")[0]==="https://www.youtube.com/watch"){
+  page = "video";
+}
+else{
+    page = null;
+}
+
 chrome.runtime.onMessage.addListener((obj, sender, res)=>{
     const { type, value, data } = obj;
-    url = link;
+    /*
     if(type === "playlist"){
         page = "playlist";
+        url = data.url;
         injectplaylist(false);
     }
     else if(type === "video"){
+        url = data.url;
         page = "video";
     }
-    else if(type === "exportplay"){
+    */
+    if(type === "exportplay"){
         exportPlay(data.title);
     }
     else if(type === "scrolldown"){
@@ -148,8 +162,3 @@ chrome.runtime.onMessage.addListener((obj, sender, res)=>{
         window.href.location = data.url;
     }
 })
-
-
-    //exportb.innerHTML = '<div class="yt-spec-button-shape-next__icon" aria-hidden="true"><yt-icon style="width: 24px; height: 24px;"><yt-icon-shape class="style-scope yt-icon"><icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z"/></svg></div></icon-shape></yt-icon-shape></yt-icon></div>';
-    //exportb.innerHTML = '<svg fill="currentcolor" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z"/></svg>';
-    //exportb.setAttribute("aria-label", "Export");
