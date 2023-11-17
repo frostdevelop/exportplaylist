b8773d1a169648bbe15f0126415c58d87ff7759d
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-11-17 16:56:51 -0700

    Update test.js

--- test.js
+++ test.js
@@ -69,6 +69,7 @@ waitForElm('div[class="metadata-buttons-wrapper style-scope ytd-playlist-header-
     yticon.appendChild(yticonshape);
     yticonshape.innerHTML = '<icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z"/></svg></div></icon-shape>';
     let scrollb = document.createElement('button');
+  	scrollb.className = classes;
     scrollb.onclick = scroll;
     buttonrenderer = document.createElement("ytd-button-renderer");
     buttonrenderer.className = "style-scope ytd-playlist-header-renderer";
