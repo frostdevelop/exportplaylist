f3864b886f559bd63db59b73db50314caabf9dfd
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-11-17 18:16:19 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var enable = true;
 function waitForElm(selector) {
     return new Promise(resolve => {
         if (document.querySelector(selector)) {
@@ -18,8 +19,15 @@ function waitForElm(selector) {
     });
 }
 
-function scroll(){
-    var scrolldown = setInterval(() => window.scrollBy(0, 2000), 200);
+function scroll(enabled=true){
+ 	if(enabled==true){
+      console.log("True");
+      var scrolldown = setInterval(() => window.scrollBy(0, 2000), 200);
+    }
+  	else{
+      console.log("False");
+      clearInterval(scrolldown);
+    }
 }
 
 function exportPlay(title = false){
@@ -70,7 +78,17 @@ waitForElm('div[class="metadata-buttons-wrapper style-scope ytd-playlist-header-
     yticonshape.innerHTML = '<icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z"/></svg></div></icon-shape>';
     let scrollb = document.createElement('button');
   	scrollb.className = classes;
-    scrollb.onclick = scroll;
+    scrollb.onclick = ()=>{
+      var enable = !enable;
+      if(enable==true){
+      	console.log("True");
+    		var scrolldown = setInterval(() => window.scrollBy(0, 2000), 200);
+    	}
+  	  else{
+      	console.log("False");
+      	clearInterval(scrolldown);
+      }
+    };
     buttonrenderer = document.createElement("ytd-button-renderer");
     buttonrenderer.className = "style-scope ytd-playlist-header-renderer";
     bar.appendChild(buttonrenderer);
@@ -86,5 +104,24 @@ waitForElm('div[class="metadata-buttons-wrapper style-scope ytd-playlist-header-
     scrollb.appendChild(icondiv);
     icondiv.appendChild(yticon);
     yticon.appendChild(yticonshape);
-    yticonshape.innerHTML = '<icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8,12 C9.10457,12 10,12.8954 10,14 C10,15.1046 9.10457,16 8,16 C6.89543,16 6,15.1046 6,14 C6,12.8954 6.89543,12 8,12 Z M8,-3.10862447e-14 C8.55229,-3.10862447e-14 9,0.447715 9,1 L9,6.58579 L10.2929,5.2929 C10.6834,4.90237 11.3166,4.90237 11.7071,5.2929 C12.0976,5.68342 12.0976,6.31658 11.7071,6.70711 L8,10.4142 L4.29289,6.70711 C3.90237,6.31658 3.90237,5.68342 4.29289,5.2929 C4.68342,4.90237 5.31658,4.90237 5.70711,5.2929 L7,6.58579 L7,1 C7,0.447715 7.44772,-3.10862447e-14 8,-3.10862447e-14 Z"/></svg></div></icon-shape>';
+    yticonshape.innerHTML = '<icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8,12 C9.10457,12 10,12.8954 10,14 C10,15.1046 9.10457,16 8,16 C6.89543,16 6,15.1046 6,14 C6,12.8954 6.89543,12 8,12 Z M8,-3.10862447e-14 C8.55229,-3.10862447e-14 9,0.447715 9,1 L9,6.58579 L10.2929,5.2929 C10.6834,4.90237 11.3166,4.90237 11.7071,5.2929 C12.0976,5.68342 12.0976,6.31658 11.7071,6.70711 L8,10.4142 L4.29289,6.70711 C3.90237,6.31658 3.90237,5.68342 4.29289,5.2929 C4.68342,4.90237 5.31658,4.90237 5.70711,5.2929 L7,6.58579 L7,1 C7,0.447715 7.44772,-3.10862447e-14 8,-3.10862447e-14 Z"/></svg></div></icon-shape>';
+  	/*let dscrollb = document.createElement('button');
+  	dscrollb.className = classes;
+    dscrollb.onclick = ()=>{scroll(false)};
+    buttonrenderer = document.createElement("ytd-button-renderer");
+    buttonrenderer.className = "style-scope ytd-playlist-header-renderer";
+    bar.appendChild(buttonrenderer);
+    shape = buttonrenderer.querySelector('yt-button-shape');
+    shape.appendChild(dscrollb);
+    tooltip = buttonrenderer.querySelector('tp-yt-paper-tooltip');
+    tooltip.innerHTML = '<div id="tooltip" class="style-scope tp-yt-paper-tooltip hidden" style-target="tooltip">Scroll</div>';
+    icondiv = document.createElement("div");
+    icondiv.className = 'yt-spec-button-shape-next__icon';
+    yticon = document.createElement("yt-icon");
+    yticonshape = document.createElement("yt-icon-shape");
+    yticonshape.className = "style-scope yt-icon";
+    dscrollb.appendChild(icondiv);
+    icondiv.appendChild(yticon);
+    yticon.appendChild(yticonshape);
+    yticonshape.innerHTML = '<icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8,12 C9.10457,12 10,12.8954 10,14 C10,15.1046 9.10457,16 8,16 C6.89543,16 6,15.1046 6,14 C6,12.8954 6.89543,12 8,12 Z M8,-3.10862447e-14 C8.55229,-3.10862447e-14 9,0.447715 9,1 L9,6.58579 L10.2929,5.2929 C10.6834,4.90237 11.3166,4.90237 11.7071,5.2929 C12.0976,5.68342 12.0976,6.31658 11.7071,6.70711 L8,10.4142 L4.29289,6.70711 C3.90237,6.31658 3.90237,5.68342 4.29289,5.2929 C4.68342,4.90237 5.31658,4.90237 5.70711,5.2929 L7,6.58579 L7,1 C7,0.447715 7.44772,-3.10862447e-14 8,-3.10862447e-14 Z"/></svg></div></icon-shape>';*/
 });
