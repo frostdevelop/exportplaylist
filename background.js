819dfcc2e99722d4a82694c638508bddabd3b6bb
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-07 13:25:13 -0700

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if(tab.url && (tab.url.includes("youtube.com/watch")){
+  if(tab.url && (tab.url.includes("youtube.com/watch"))){
     chrome.tabs.sendMessage(tabId, {
       type: "video",
       data: {
@@ -7,7 +7,7 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     },
     });
   }
-  else if(tab.url && (tab.url.includes("youtube.com/playlist")){
+  else if(tab.url && (tab.url.includes("youtube.com/playlist"))){
     chrome.tabs.sendMessage(tabId, {
       type: "playlist",
       data: {
@@ -15,4 +15,4 @@ chrome.tabs.onUpdated.addListener((tabId, tab) => {
     },
     });
   }
-}
+})
