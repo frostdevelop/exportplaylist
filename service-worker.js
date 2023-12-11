b241302112d939a4ad21dd67fd2e1e8d846b5cad
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-11 12:07:34 -0700

    Added function

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,10 @@
+async function sendMessageToActiveTab(message) {
+  const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+  const response = await chrome.tabs.sendMessage(tab.id, message);
+  // do something with response here, not outside the function
+  console.log(response);
+}
+
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
   if(tab.url && (tab.url.includes("youtube.com/watch"))){
     chrome.tabs.sendMessage(tabId, {
