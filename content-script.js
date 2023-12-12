d1f4128d0d4de2b4f3f39629e2cc30d74e072644
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-12 07:41:53 -0700

    Format and update content script from testing

--- content-script.js
+++ content-script.js
@@ -2,18 +2,15 @@
 let url = location.href.split("?")[0];
 let page;
 
-function scroll(){
+function scroll() {
     var scrolldown = setInterval(() => {
         window.scrollBy(0, 2000);
     }, 200);
 }
 
-function exportPlay(title = false){
-    if(scrolldown){
-        clearInterval(scrolldown);
-    };
+function exportPlay(title = false) {
     let videos = [];
-    videos.push(document.querySelector('yt-formatted-string[class="style-scope yt-dynamic-sizing-formatted-string yt-sans-28"]').innerHTML);
+    videos.push(document.querySelector('div[class="dynamic-text-container style-scope yt-dynamic-sizing-formatted-string"]').querySelector('yt-formatted-string').innerHTML);
     const links = document.querySelectorAll('a');
     for (const link of links) {
         if (link.id === "video-title") {
@@ -23,7 +20,9 @@ function exportPlay(title = false){
     }
 
     let data = videos.join('\n');
-    let blob = new Blob([data], {type: 'text/csv'});
+    let blob = new Blob([data], {
+        type: 'text/csv'
+    });
     let elem = window.document.createElement('a');
     elem.href = window.URL.createObjectURL(blob);
     elem.download = videos[0].replace(/\s/g, '') + '.csv';
@@ -53,66 +52,77 @@ function waitForElm(selector) {
     });
 }
 
-function savePlaylist(listname){
+function savePlaylist(listname) {
     document.querySelectorAll('[aria-label="Save to playlist"]')[0].click()
-    waitForElm('tp-yt-paper-dialog').then(()=>{
-    let checks = document.querySelectorAll('tp-yt-paper-checkbox[class="style-scope ytd-playlist-add-to-option-renderer"]')
-    let found = false;
-    for (let i=0;i<checks.length;i++){
-      if(checks[i].childNodes[2].children[0].children[0].children[0].title==listname){
-        checks[i].click();
-        found = true;
-      }
-    }
-    if(found == false){
-        document.querySelector('tp-yt-paper-item[class="style-scope ytd-compact-link-renderer"]').click();
-        let elm = document.querySelector('input[placeholder="Enter playlist name..."]');
-        elm.value=listname;
-        let ev = new Event("input");
-        elm.parentElement.dispatchEvent(ev);
-        document.querySelectorAll('button[aria-label="Create"]')[1].click();
-    }
-    }
-    );
+    waitForElm('tp-yt-paper-dialog').then(() => {
+        let checks = document.querySelectorAll('tp-yt-paper-checkbox[class="style-scope ytd-playlist-add-to-option-renderer"]')
+        let found = false;
+        for (let i = 0; i < checks.length; i++) {
+            if (checks[i].childNodes[2].children[0].children[0].children[0].title == listname) {
+                checks[i].click();
+                found = true;
+            }
+        }
+        if (found == false) {
+            document.querySelector('tp-yt-paper-item[class="style-scope ytd-compact-link-renderer"]').click();
+            let elm = document.querySelector('input[placeholder="Enter playlist name..."]');
+            elm.value = listname;
+            let ev = new Event("input");
+            elm.parentElement.dispatchEvent(ev);
+            document.querySelectorAll('button[aria-label="Create"]')[1].click();
+        }
+    });
 }
 
-function injectplaylist(title){
+function injectplaylist(title) {
     let classes = "yt-spec-button-shape-next yt-spec-button-shape-next--tonal yt-spec-button-shape-next--overlay yt-spec-button-shape-next--size-m yt-spec-button-shape-next--icon-button";
-    waitForElm('div[class="metadata-buttons-wrapper style-scope ytd-playlist-header-renderer"]').then(()=>{
+    waitForElm('div[class="metadata-buttons-wrapper style-scope ytd-playlist-header-renderer"]').then(() => {
+        function scroll() {
+            document.querySelector('div[id="contents"][class=" style-scope ytd-playlist-video-list-renderer style-scope ytd-playlist-video-list-renderer"]').querySelector('ytd-continuation-item-renderer').scrollIntoView();
+            const endobserver = new MutationObserver(mutations => {
+                if (document.querySelector('div[id="contents"][class=" style-scope ytd-playlist-video-list-renderer style-scope ytd-playlist-video-list-renderer"]').querySelector('ytd-continuation-item-renderer') === null) {
+                    endobserver.disconnect();
+                    console.log("disconnect");
+                    exportPlay(false);
+                    scrollb.click();
+                };
+                document.querySelector('div[id="contents"][class=" style-scope ytd-playlist-video-list-renderer style-scope ytd-playlist-video-list-renderer"]').querySelector('ytd-continuation-item-renderer').scrollIntoView();
+            });
+            endobserver.observe(document.querySelector('div[id="contents"][class=" style-scope ytd-playlist-video-list-renderer style-scope ytd-playlist-video-list-renderer"]'), {
+                childList: true,
+                subtree: true
+            });
+            scrollb.addEventListener("click", () => {
+                console.log(false);
+                clearInterval(scrolldown);
+                scrollb.addEventListener("click", scroll, {
+                    once: true
+                });
+            }, {
+                once: true
+            });
+        };
+
         let bar = document.querySelector('div[class="metadata-buttons-wrapper style-scope ytd-playlist-header-renderer"]');
-        let exportb = document.createElement('button');
-        exportb.className = classes;
-        exportb.onclick = ()=>exportPlay(false);
+        let menuelm = bar.querySelector('ytd-menu-renderer');
+
+        let scrollb = document.createElement('button');
+        scrollb.className = classes;
+        scrollb.addEventListener("click", scroll, {
+            once: true
+        });
+
         let buttonrenderer = document.createElement("ytd-button-renderer");
         buttonrenderer.className = "style-scope ytd-playlist-header-renderer";
-        bar.appendChild(buttonrenderer);
+        bar.insertBefore(buttonrenderer, menuelm);
+        let shape = buttonrenderer.querySelector('yt-button-shape');
+        shape.appendChild(scrollb);
         let tooltip = buttonrenderer.querySelector('tp-yt-paper-tooltip');
         tooltip.innerHTML = '<div id="tooltip" class="style-scope tp-yt-paper-tooltip hidden" style-target="tooltip">Export</div>';
-        let shape = buttonrenderer.querySelector('yt-button-shape');
-        shape.appendChild(exportb); 
         let icondiv = document.createElement("div");
         icondiv.className = 'yt-spec-button-shape-next__icon';
         let yticon = document.createElement("yt-icon");
         let yticonshape = document.createElement("yt-icon-shape");
-        yticonshape.className = "style-scope yt-icon";
-        exportb.appendChild(icondiv);
-        icondiv.appendChild(yticon);
-        yticon.appendChild(yticonshape);
-        yticonshape.innerHTML = '<icon-shape class="yt-spec-icon-shape"><div style="width: 100%; height: 100%; fill: currentcolor;"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M8.71,7.71,11,5.41V15a1,1,0,0,0,2,0V5.41l2.29,2.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-4-4a1,1,0,0,0-.33-.21,1,1,0,0,0-.76,0,1,1,0,0,0-.33.21l-4,4A1,1,0,1,0,8.71,7.71ZM21,14a1,1,0,0,0-1,1v4a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V15a1,1,0,0,0-2,0v4a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V15A1,1,0,0,0,21,14Z"/></svg></div></icon-shape>';
-        let scrollb = document.createElement('button');
-        scrollb.innerHtml = "Load videos";
-        scrollb.onclick = scroll;
-        buttonrenderer = document.createElement("ytd-button-renderer");
-        buttonrenderer.className = "style-scope ytd-playlist-header-renderer";
-        bar.appendChild(buttonrenderer);
-        shape = buttonrenderer.querySelector('yt-button-shape');
-        shape.appendChild(scrollb);
-        tooltip = buttonrenderer.querySelector('tp-yt-paper-tooltip');
-        tooltip.innerHTML = '<div id="tooltip" class="style-scope tp-yt-paper-tooltip hidden" style-target="tooltip">Scroll</div>';
-        icondiv = document.createElement("div");
-        icondiv.className = 'yt-spec-button-shape-next__icon';
-        yticon = document.createElement("yt-icon");
-        yticonshape = document.createElement("yt-icon-shape");
         yticonshape.className = "style-scope yt-icon";
         scrollb.appendChild(icondiv);
         icondiv.appendChild(yticon);
@@ -121,19 +131,21 @@ function injectplaylist(title){
     });
 }
 
-if(location.href.split("?")[0]==="https://www.youtube.com/playlist"){
+if (location.href.split("?")[0] === "https://www.youtube.com/playlist") {
     page = "playlist";
     injectplaylist(false);
-}
-else if(location.href.split("?")[0]==="https://www.youtube.com/watch"){
+} else if (location.href.split("?")[0] === "https://www.youtube.com/watch") {
     page = "video";
-}
-else{
+} else {
     page = null;
 }
 
-chrome.runtime.onMessage.addListener((obj, sender, res)=>{
-    const { type, value, data } = obj;
+chrome.runtime.onMessage.addListener((obj, sender, res) => {
+    const {
+        type,
+        value,
+        data
+    } = obj;
     /*
     if(type === "playlist"){
         page = "playlist";
@@ -145,19 +157,15 @@ chrome.runtime.onMessage.addListener((obj, sender, res)=>{
         page = "video";
     }
     */
-    if(type === "exportplay"){
+    if (type === "exportplay") {
         exportPlay(data.title);
-    }
-    else if(type === "scrolldown"){
+    } else if (type === "scrolldown") {
         scroll();
-    }
-    else if(type === "stopscroll"){
+    } else if (type === "stopscroll") {
         clearInterval(scrolldown);
-    }
-    else if(type === "import"){
+    } else if (type === "import") {
         savePlaylist(data.name);
-    }
-    else if(type === "nav"){
+    } else if (type === "nav") {
         window.href.location = data.url;
     }
 })
