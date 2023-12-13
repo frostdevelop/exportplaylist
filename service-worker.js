b9c491ed71b5d80752bdffb9e41c087993efbbdf
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2023-12-12 22:45:09 -0700

    Removing unneccessary coe

--- service-worker.js
+++ service-worker.js
@@ -2,24 +2,7 @@ async function sendMessageToActiveTab(message) {
   const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
   const response = await chrome.tabs.sendMessage(tab.id, message);
   // do something with response here, not outside the function
-  console.log(response);
+    return response;
 }
 
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if(tab.url && (tab.url.includes("youtube.com/watch"))){
-    chrome.tabs.sendMessage(tabId, {
-      type: "video",
-      data: {
-        url: tab.url,
-    },
-    });
-  }
-  else if(tab.url && (tab.url.includes("youtube.com/playlist"))){
-    chrome.tabs.sendMessage(tabId, {
-      type: "playlist",
-      data: {
-        url: tab.url,
-    },
-    });
-  }
-})
+chrome.tabs.onUpdated.addListener((tabId, tab) => {})
