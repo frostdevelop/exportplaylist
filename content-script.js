1fc2a3543609e2e3cfcd9068f3f8a2add31eb163
Pacifiky <142683933+Pacifiky@users.noreply.github.com>
2024-02-02 20:07:43 +0530

    Testing

--- content-script.js
+++ content-script.js
@@ -123,6 +123,62 @@ async function savePlaylist(listname) {
     console.log(document.querySelector('yt-text-input-form-field-renderer').value)
 }
 
+/*
+function savePlaylist(listname) {
+    waitForElm('[aria-label="Save to playlist"]').then((e) => {
+        e.click()
+        waitForElm('tp-yt-paper-dialog').then(() => {
+            let checks = document.querySelectorAll('tp-yt-paper-checkbox');
+            let found = false;
+            for (let i = 0; i < checks.length; i++) {
+                if (checks[i].children[1].children[0].children[0].children[0].title == listname) {
+                    console.log(checks[i].checked);
+                    if (checks[i].checked === false) {
+                        checks[i].click();
+                    };
+                    if (checks[i].checked === true) {
+                        console.log("checked")
+                        chrome.runtime.sendMessage({
+                            type: "finishimport"
+                        });
+                    };
+                    found = true;
+                    console.log("found");
+                }
+            }
+            console.log("aftercheck")
+            if (found == false) {
+                console.log("notfound")
+                waitForElm('ytd-add-to-playlist-create-renderer').then(() => {
+                    document.getElementsByClassName("ytd-add-to-playlist-create-renderer")[0].click();
+                    console.log("clicked");
+                    let elm = document.getElementsByClassName("tp-yt-paper-input")[3];
+                    elm.value = listname;
+                    elm.parentElement.dispatchEvent(new Event("input"));
+                    document.querySelectorAll('button[aria-label="Create"]')[1].click();
+                });
+            }
+            const waitNotif = new MutationObserver(mutations => {
+                let notifs = document.querySelectorAll("tp-yt-paper-toast");
+                for (let i = 0; i < notifs.length; i++) {
+                    if (notifs[i].children[1].firstElementChild.firstElementChild && notifs[i].children[1].firstElementChild.firstElementChild.innerHTML === "Saved to ") {
+                        chrome.runtime.sendMessage({
+                            type: "finishimport"
+                        });
+                        waitNotif.disconnect();
+                    }
+                }
+            });
+
+            waitNotif.observe(document.querySelector("ytd-popup-container"), {
+                childList: true,
+                subtree: true
+            });
+        });
+    });
+}
+*/
+
 function injectplaylist(title) {
     if (document.getElementById('scrollb')) {
         return
